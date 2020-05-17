04623ad9d5f79d545963eb2fe203063f726bb73a
Smit1400 <46996928+Smit1400@users.noreply.github.com>
2020-05-17 20:34:54 +0530

    task list page and form page added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:todoist/app/landing_page.dart';
+import 'package:todoist/app/lists/add_list_page.dart';
+import 'package:todoist/app/lists/display_list_of_user.dart';
 
 void main() => runApp(MaterialApp(
       home: MyApp(),
@@ -8,6 +10,6 @@ void main() => runApp(MaterialApp(
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return LandingPage();
+    return DisplayListOfUser();
   }
 }
